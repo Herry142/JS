eaed8ebd65baeb371516a7a975c9b26ed3589cf3
Herry142 <antoha.veleshchyk@gmail.com>
2023-07-07 22:28:23 +0300

    1

--- JS/script.js
+++ JS/script.js
@@ -2,4 +2,70 @@
 let Hellow = "Привіт";
 
 
-alert(Hellow);
+console.log(Hellow);
+
+const User = true;
+
+// if and else operatir
+
+// if (User) {
+//     alert('Hellow')
+// } else {
+//     alert('Good')
+// };
+
+
+// ? and : (tearnary)operator
+// User ? alert('bed') : alert('Good');
+
+let age = 31;
+let name = "Igor";
+
+
+// switch operator
+// switch(name) {
+//     case 10:
+//         alert("Heppy birthday>age10");
+//         break;
+//     case "Igor":
+//         alert("Heppy birthday>Igor");
+//         break;
+//     case "Oleg":
+//         alert("Heppy birthday>Oleg");
+//         break;
+//     case 20:
+//         alert("Heppy birthday>age20");
+//         break;
+//     case 30:
+//         alert("Heppy birthday>age30");
+//         break;
+//     default:
+//         alert("go home")
+// }
+
+
+// Arithmetic operator
+// const nam1 = 10;
+// const nam2 = 4;
+// console.log(nam1 + nam2);
+// console.log(nam1 - nam2);
+// console.log(nam1 * nam2);
+// console.log(nam1 / nam2);
+// console.log(nam1 % nam2);
+
+
+// Assignment operator
+// let nam3 = 40;
+// nam3 += 30;
+// console.log(nam3);
+
+
+// Comparison operator
+
+let User1 = 11;
+if (User1 > 12) {
+    alert('Hellow')
+}
+else{
+    console.log('HELLOW')
+}
